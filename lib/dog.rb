7fd6296f9d3cea7857b4ff110d0a9eb7f6caacac
Jaryd Hermann <hermannjaryd@gmail.com>
2021-02-26 11:48:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,14 @@
 # Add your code here
+def Class Dog
+attr_accessor :name
+@@all = [] #class variable 
+
+def initialize(name)
+  @name = name 
+end 
+
+def self.all 
+  @all 
+end 
+
+end #end of class 
